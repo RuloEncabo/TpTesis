d973941bdb12ca67113f523e872b1aebc5ecd293
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-09 20:04:22 -0300

    Update views.py

--- EltaRRHH/usuarios/views.py
+++ EltaRRHH/usuarios/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from .forms import RegistroForm
 # Create your views here.
+
 def registro(request):
-    return render(request, 'usuarios/registro.html')
+
+    form = RegistroForm()
+
+    context={
+        
+        'form':form
+        
+    }
+    #envia el diccionario context que tiene la estructura del formulario creado 
+    return render(request, 'usuarios/registro.html',context)
 
 def login(request):
     return render(request, 'usuarios/login.html')
