304bb7c0f64722b9f988207c5236d683e76597d9
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-08 21:36:39 -0300

    Update admin.py

--- EltaRRHH/chofer/admin.py
+++ EltaRRHH/chofer/admin.py
@@ -1,14 +1,3 @@
 from django.contrib import admin
-from .models import Chofer
+
 # Register your models here.
-
-class ChoferAdmin (admin.ModelAdmin):
-    
-    list_display = ('nombre', 'apellido','dni','movil')
-    prepopulated_fields = {'slug': ('apellido',)}
-    search_fields = ['nombre']
-
-#registar la entidad
-
-admin.site.register (Chofer, ChoferAdmin)
-    
