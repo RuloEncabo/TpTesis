978f4cb26daefa641f68a576dbaf843fc0663ca7
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-08 21:36:45 -0300

    Update models.py

--- EltaRRHH/chofer/models.py
+++ EltaRRHH/chofer/models.py
@@ -7,29 +7,23 @@ class Chofer(models.Model):
     Legajo = models.IntegerField()
     nombre = models.CharField(max_length=100)
     apellido= models.CharField(max_length=100)
-    slug = models.CharField(max_length=100)
-    dni = models.CharField(max_length=10, unique=True)
-    calle = models.CharField(max_length=100, unique=True)
-    nrocalle = models.CharField(max_length=10)
-    piso = models.CharField(max_length=10)
-    Deptartamento = models.CharField(max_length=10)
-    barrio = models.CharField(max_length=50)
-    localidad = models.CharField(max_length=80)
-    provincia = models.CharField(max_length=10)
-    cp = models.IntegerField()
-    movil = models.CharField(max_length=15)
-    contactoemergencia = models.CharField(max_length=80)
-    parentesco = models.CharField(max_length=80,blank=True)
-    foto = models.ImageField(upload_to='fotochofer/', blank=True)   
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now=True)
+    dni= models.CharField(max_length=10, unique=True)
+    calle= models.CharField(max_length=100, unique=True)
+    nrocalle=models.CharField(max_length=10)
+    piso= models.CharField(max_length=10)
+    Deptartamento= models.CharField(max_length=10)
+    barrio= models.CharField(max_length=50)
+    localidad=models.CharField(max_length=80)
+    provincia= models.CharField(max_length=10)
+    cp=models.IntegerField()
+    movil= models.CharField(max_length=15)
+    contactoemergencia=models.CharField(max_length=80)
+    parentesco=models.CharField(max_length=80,blank=True)
+    foto=models.ImageField(upload_to='fotochofer/', blank=True)
+    
+    
+    date_joined= models.DateTimeField(auto_now_add=True)
+    last_login= models.DateTimeField(auto_now=True)
     is_staff = models.BooleanField(default=False)
-    is_activo = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     
-    
-def __str__(self):
-    return self.apellido
-
-#usario, procreso, fecha, datos anteior, dato actual 
-
-#control lote de movimeintos.
