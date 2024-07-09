e024d73342422d9a1f0b69ef625ab92d45f65781
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-09 18:01:38 -0300

    Update urls.py

--- EltaRRHH/usuarios/urls.py
+++ EltaRRHH/usuarios/urls.py
@@ -4,7 +4,7 @@ from. import views
 urlpatterns = [
     
     path('registro/', views.registro, name='registro'),     #funcion def que registra un usuario
-    path('login/', views.registro, name='login'),
-    path('logout/', views.registro, name='logout'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     
 ]
