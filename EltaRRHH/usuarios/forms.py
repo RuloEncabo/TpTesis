19211fcf07ce4964f9a69d66a991cf2eb298e71a
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-21 17:19:39 -0300

    Update forms.py

--- EltaRRHH/usuarios/forms.py
+++ EltaRRHH/usuarios/forms.py
@@ -1,35 +1,43 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
 
-
-
-#la funcion se crea el formulario desde djangp 
 class RegistroForm(forms.ModelForm):
-    
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-            'placeholder':'Ingrese Clave',
-            'class':'form-control',   
-    }))
-    
-    #campo para confirmar la contraseña 
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placehoder':'Confirmar Clave',
-        'class':'form-control',
+        
+        first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Ingrese Nombre',
+        'class': 'form-control',
+        }))
+        last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Ingrese Apellido',
+        'class': 'form-control',
+        }))
+        phone_number = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Ingrese Celular',
+        'class': 'form-control',
+        }))
+        password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Ingrese Clave',
+        'class': 'form-control',
+        }))
+        email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Ingrese E-Mail',
+        'class': 'form-control',
+        }))
+        confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirmar Clave',
+        'class': 'form-control',
         }))
 
-    class Meta:
-        model = Usuario
-        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+        class Meta:
+                model = Usuario  # Aquí se especifica el modelo asociado
+                fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+        def clean(self):
+                cleaned_data = super(RegistroForm, self).clean()
+                password = cleaned_data.get('password')
+                confirm_password = cleaned_data.get('confirm_password')
         
-#aplicar los estilos a todas las cajas 
-def __init__(self,*args,**kwargs):
-    super(RegistroForm,self).__init__(*args,**kwargs)
-    
-    self.fields['first_name'].widget.attrs['placeholder'] = 'Ingrese Nombre'
-    self.fields['last_name'].widget.attrs['placeholder'] = 'Ingrese Apellido'
-    self.fields['phone_number'].widget.attrs['placeholder'] = 'Ingrese celular'
-    self.fields['email'].widget.attrs['placeholder'] = 'Correo Electronico'
-    #bucle para aplicar los estilos en las cajas de texto
-    for field in self.fields:
-        self.fields[field].widget.attrs['class'] = 'form-control' #se instancia a cada caja de texto
+                if password != confirm_password:
+                        raise forms.ValidationError(
+                        "Las contraseñas no coinciden"
+                )
