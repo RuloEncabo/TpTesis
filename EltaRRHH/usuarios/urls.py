1c2c5da9fcd20520f84ba8ed3de2b2675a0769bb
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-21 17:18:26 -0300

    Update urls.py

--- EltaRRHH/usuarios/urls.py
+++ EltaRRHH/usuarios/urls.py
@@ -3,8 +3,14 @@ from. import views
 
 urlpatterns = [
     
+    path('home/', views.home, name='home'),
     path('registro/', views.registro, name='registro'),     #funcion def que registra un usuario
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('listar/', views.listar, name='listar'),
+   #path('admin/', views.home, name='admin'),
+    path('chofer/', views.chofer, name='chofer'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'), # ruta para la funcion activate con los parametros  
+    
     
 ]
