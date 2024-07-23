0fbaa3cc92e3795a3c6b79a3995472b7d030c57e
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-23 15:45:54 -0300

    Update models.py

--- EltaRRHH/TipoKilometro/models.py
+++ EltaRRHH/TipoKilometro/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 
-class Movimiento(models.Model):
-    # Definición del modelo Movimiento (asumiendo que ya está definido)
-    mov_id = models.AutoField(primary_key=True)
-    #tipo_kilometro = models.ForeignKey('TipoKilometro', on_delete=models.CASCADE, related_name='movimiento', null=True, blank=True)
-
-    def __str__(self):
-        return str(self.mov_id)  # O el campo que desees usar como representación
 
 class TipoKilometro(models.Model):
-    tk_nombre = models.CharField(max_length=60, blank=True, null=True)
-    
+    idTipoKilometro = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=255)
+
     def __str__(self):
-        return self.tk_nombre
+        return self.descripcion
