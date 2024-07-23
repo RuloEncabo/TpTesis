0b3d885b314b890f1680164f601327e7bc7dabc2
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-23 15:26:11 -0300

    Update urls.py

--- EltaRRHH/chofer/urls.py
+++ EltaRRHH/chofer/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .import views
+from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name="home")
+    path('regchofer/', views.regchofer, name='regchofer'),
+]
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Si necesitas servir archivos estáticos y medios en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
