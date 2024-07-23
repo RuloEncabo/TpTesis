5e96fedbd0e1b805fadd7b47c66e6b5b537854a4
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-23 15:26:19 -0300

    Update views.py

--- EltaRRHH/chofer/views.py
+++ EltaRRHH/chofer/views.py
@@ -1,13 +1,60 @@
 from django.shortcuts import render, redirect
-from .forms import ChoferForm
-from .models import Chofer
+from django.contrib.auth import login
+from .forms import RegistroForm, ChoferForm
+from .models import Usuario, Chofer
 
-def registrar_chofer(request):
+def registro_chofer(request):
     if request.method == 'POST':
-        form = ChoferForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('alguna_vista_de_exito')  # Redirigir a una vista de éxito
+        form = RegistroForm(request.POST)
+        chofer_form = ChoferForm(request.POST, request.FILES)
+        if form.is_valid() and chofer_form.is_valid():
+            email = form.cleaned_data['email']
+            
+            user, created = Usuario.objects.get_or_create(
+                email=email,
+                defaults={
+                    'first_name': form.cleaned_data['first_name'],
+                    'last_name': form.cleaned_data['last_name'],
+                    'username': email.split("@")[0],
+                    'phone_number': form.cleaned_data['phone_number'],
+                    'password': form.cleaned_data['password']
+                }
+            )
+            
+            if created:
+                # Crear el chofer solo si el usuario es nuevo
+                chofer = chofer_form.save(commit=False)
+                chofer.usuario = user
+                chofer.save()
+
+                # Iniciar sesión automáticamente
+                login(request, user)
+
+                # Redirigir a una página de éxito
+                return redirect('success_page')  # Cambia 'success_page' por la URL adecuada
+            else:
+                # El usuario ya existe, no necesitas guardar los datos personales
+                chofer = chofer_form.save(commit=False)
+                chofer.usuario = user
+                chofer.save()
+                
+                # Redirigir o mostrar un mensaje que indique que el usuario ya está registrado
+                return redirect('success_page')  # Cambia 'success_page' por la URL adecuada
+
     else:
-        form = ChoferForm()
-    return render(request, 'chofer/rregChofer.html', {'form': form})
+        form = RegistroForm()
+        chofer_form = ChoferForm()
+
+    context = {
+        'form': form,
+        'chofer_form': chofer_form,
+        'user_exists': False
+    }
+
+    # Verifica si el usuario ya existe basándote en el correo electrónico
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        if email and Usuario.objects.filter(email=email).exists():
+            context['user_exists'] = True
+
+    return render(request, 'chofer/regChoferistro.html', context)
