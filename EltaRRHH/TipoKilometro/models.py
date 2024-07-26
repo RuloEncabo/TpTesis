88b2ff114832e8143b06c2be9949982c1459886d
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-26 16:27:50 -0300

    Update models.py

--- EltaRRHH/TipoKilometro/models.py
+++ EltaRRHH/TipoKilometro/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class TipoKilometro(models.Model):
-    idTipoKilometro = models.AutoField(primary_key=True, default=0)
+    idTipoKilometro = models.AutoField(primary_key=True)
     descripcion = models.CharField(max_length=255, default="")
 
     def __str__(self):
