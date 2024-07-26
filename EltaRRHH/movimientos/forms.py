7cd35c3341202fcb676028d535673feabca9e4c4
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-26 16:27:35 -0300

    Update forms.py

--- EltaRRHH/movimientos/forms.py
+++ EltaRRHH/movimientos/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import Movimiento,TipoKilometro
+from .models import Movimientos, TipoKilometros
 
-class MovimientoForm(forms.ModelForm):
-    tipo_kilometro = forms.ModelChoiceField(queryset=TipoKilometro.objects.all())
+class MovimientosForm(forms.ModelForm):
+    tipo_kilometro = forms.ModelChoiceField(queryset=TipoKilometros.objects.all())
 
     class Meta:
-        model = Movimiento
+        model = Movimientos
         fields = ['nFlota', 'inicio', 'fin', 'kmInicio', 'kmFin', 'lugar_inicio', 
                     'lugar_fin', 'tipo_kilometro', 'lleva_carga', 'permanencia', 
                     'diasPermanencia', 'cruce_frontera', 'comentarios']
