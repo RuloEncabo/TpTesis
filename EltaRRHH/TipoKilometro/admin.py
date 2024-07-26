faedfec741164cf5f2cee6068da7224bc606db39
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-07-26 06:27:18 -0300

    Update admin.py

--- EltaRRHH/TipoKilometro/admin.py
+++ EltaRRHH/TipoKilometro/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import TipoKilometro
+from django.utils.html import format_html
+
+
+class TipoKmAdim(admin.ModelAdmin):
+    #lo que muestra en la tabla de usuarios 
+    list_display = ('idTipoKilometro','descripcion')
+
+
 
 # Register your models here.
+admin.site.register(TipoKilometro, TipoKmAdim)
