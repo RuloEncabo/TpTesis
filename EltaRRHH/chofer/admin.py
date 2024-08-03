68cf99815daf8e0800aa68d656b10cd6cf271283
Marcelo Encabo <89429924+RuloEncabo@users.noreply.github.com>
2024-08-03 08:10:59 -0300

    Update admin.py

--- EltaRRHH/chofer/admin.py
+++ EltaRRHH/chofer/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin
+from .models import Chofer
+from django.utils.html import format_html
+
 # Register your models here.
+class ChoferAdmin(admin.ModelAdmin):
+    #control que permita adinistrar mis imagenes
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="border-radius:50%;" >'.format(object.foto.url))
+    
+    thumbnail.short_description = 'Imagen de Perfil'
+    list_display =('thumbnail' , 'usuario', 'dni', 'direccion', 'localidad', 'provincia', 'ingresoFCA_venc', 'licencia_venc','psicofisico_venc', 'curso_venc')
+
+admin.site.register(Chofer, ChoferAdmin)
