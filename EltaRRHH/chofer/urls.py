cb6ce63d178da881a91c9bd7bb567859e07ba084
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-08-03 22:39:19 -0300

    Update urls.py

--- EltaRRHH/chofer/urls.py
+++ EltaRRHH/chofer/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('listchofer/', views.listchofer, name='listchofer'),
     path('listdoc/', views.listdoc, name='listdoc'),
-    path('modidoc/<int:id>/', views.modidoc, name='modidoc'),
+    path('modidoc/', views.modidoc, name='modidoc'),
 ]
 
 if settings.DEBUG:
