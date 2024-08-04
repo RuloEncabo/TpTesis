90583d302c08a75941616579f8350895713a8a90
Marcelo Encabo <marcelo.encabo@hotmail.com>
2024-08-04 20:26:57 -0300

    Update views.py

--- EltaRRHH/movimientos/views.py
+++ EltaRRHH/movimientos/views.py
@@ -52,41 +52,7 @@ def registrarmovimiento(request):
 
     return render(request, 'movimientos/registrarmovimiento.html', {'form': form})
 
-"""@login_required
-def registrarmovimiento(request):
-    if request.method == 'POST':
-        form = MovimientosForm(request.POST)
-        if form.is_valid():
-            usuario = request.user
-            if usuario.role == 'chofer':
-                try:
-                    usuario_chofer = UsuarioChofer.objects.get(usuario=usuario)
-                    Movimientos.objects.create(
-                        usuario=usuario,
-                        usuario_chofer=usuario_chofer,
-                        nFlota=form.cleaned_data['nFlota'],
-                        inicio=form.cleaned_data['inicio'],
-                        fin=form.cleaned_data['fin'],
-                        kmInicio=form.cleaned_data['kmInicio'],
-                        kmFin=form.cleaned_data['kmFin'],
-                        lugar_inicio=form.cleaned_data['lugar_inicio'],
-                        lugar_fin=form.cleaned_data['lugar_fin'],
-                        lleva_carga=form.cleaned_data['lleva_carga'],
-                        permanencia=form.cleaned_data['permanencia'],
-                        diasPermanencia=form.cleaned_data['diasPermanencia'],
-                        cruce_frontera=form.cleaned_data['cruce_frontera'],
-                        comentarios=form.cleaned_data['comentarios'],
-                    )
-                    return redirect('movimiento_lista')
-                except UsuarioChofer.DoesNotExist:
-                    form.add_error(None, "El usuario no es un chofer registrado.")
-            else:
-                form.add_error(None, "El usuario no tiene el rol de chofer.")
-    else:
-        form = MovimientosForm()
-    
-    return render(request, 'movimientos/registrarmovimiento.html', {'form': form})
-"""
+
 ### Lista los campos del Chofer ###
 def listmovimiento(request):
     movimientos = Movimientos.objects.all()
@@ -98,14 +64,17 @@ def listmovimiento(request):
 
 ### Lista los campos del Chofer que esta logeado ###
 @login_required
+@login_required
 def listmovimientoChofer(request):
     usuario = request.user
     try:
+        # Obtener el chofer asociado al usuario registrado
         chofer = Chofer.objects.get(usuario=usuario)
+        # Filtrar movimientos del chofer
         movimientos = Movimientos.objects.filter(chofer=chofer)
-    except UsuarioChofer.DoesNotExist:
+    except Chofer.DoesNotExist:
+        # Si no existe el chofer, no hay movimientos para mostrar
         movimientos = Movimientos.objects.none()
-        # O podrías redirigir a una página de error si prefieres
 
     # Calcular la diferencia entre kmInicio y kmFin para cada movimiento
     for movimiento in movimientos:
@@ -148,49 +117,3 @@ def listusuariochofer(request):
     }
     return render(request, 'chofer/listusuariochofer.html', context)
     
-"""
-@login_required
-def registrar_movimientoViaje(request):
-    if request.method == 'POST':
-        form = MovimientoForm(request.POST)
-        if form.is_valid():
-            usuario = request.user
-            if usuario.role == 'chofer':
-                try:
-                    usuario_chofer = UsuarioChofer.objects.get(usuario=usuario)
-                    viaje_activo = Viaje.objects.filter(chofer=usuario_chofer, activo=True).first()
-                    if not viaje_activo:
-                        form.add_error(None, "No hay un viaje activo para registrar el movimiento.")
-                    else:
-                        tipo_kilometro = form.cleaned_data['tipo_kilometro']
-                        movimiento = Movimiento(
-                            chofer=usuario_chofer,
-                            viaje=viaje_activo,
-                            nFlota=form.cleaned_data['nFlota'],
-                            inicio=form.cleaned_data['inicio'],
-                            fin=form.cleaned_data['fin'],
-                            kmInicio=form.cleaned_data['kmInicio'],
-                            kmFin=form.cleaned_data['kmFin'],
-                            lugar_inicio=form.cleaned_data['lugar_inicio'],
-                            lugar_fin=form.cleaned_data['lugar_fin'],
-                            tipo_kilometro=tipo_kilometro,
-                            lleva_carga=form.cleaned_data['lleva_carga'],
-                            permanencia=form.cleaned_data['permanencia'],
-                            diasPermanencia=form.cleaned_data['diasPermanencia'],
-                            cruce_frontera=form.cleaned_data['cruce_frontera'],
-                            comentarios=form.cleaned_data['comentarios'],
-                        )
-                        movimiento.clean()  # Llama al método clean para validar el movimiento
-                        movimiento.save()
-                        return redirect('movimiento_lista')
-                except UsuarioChofer.DoesNotExist:
-                    form.add_error(None, "El usuario no es un chofer registrado.")
-                except ValidationError as e:
-                    form.add_error(None, e.message)
-            else:
-                form.add_error(None, "El usuario no tiene el rol de chofer.")
-    else:
-        form = MovimientoForm()
-    
-    return render(request, 'movimientos/registrar_movimiento.html', {'form': form})
-"""
