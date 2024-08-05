b248577ac1e87af6657524c6c6843c4a40bd7cc5
Marcelo Encabo <89429924+RuloEncabo@users.noreply.github.com>
2024-08-05 17:35:48 -0300

    Update admin.py

--- EltaRRHH/movimientos/admin.py
+++ EltaRRHH/movimientos/admin.py
@@ -3,7 +3,7 @@ from .models import Movimientos
 
 # Register your models here.
 class movimentosAdmin(admin.ModelAdmin):
-    
+    #control que permita adinistrar mis imagenes
     
     list_display =('chofer' , 'nFlota', 'inicio', 'kmInicio', 'lugar_inicio', 'tipo_kilometro', 'fin', 'kmFin',
                     'lugar_fin', 'permanencia', 'diasPermanencia', 'cruce_frontera', 'comentarios', 'created_at')
