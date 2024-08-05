0e11b6b0333c56b963c61a7d71caa6b74d708d26
Marcelo Encabo <89429924+RuloEncabo@users.noreply.github.com>
2024-08-05 15:32:31 -0300

    Update urls.py

--- EltaRRHH/movimientos/urls.py
+++ EltaRRHH/movimientos/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('listmovimiento/', views.listmovimiento, name='listmovimiento'),
     path('movimiento/', views.movimiento, name='movimiento'),
     path('listmovimientoChofer/', views.listmovimientoChofer, name='listmovimientoChofer'),
+    path('analitica/', views.analitica, name='analitica'),
 ]
