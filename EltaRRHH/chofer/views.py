c6f3240ba91a42e0a9baaed7a9eb9ae15bee602f
Marcelo Encabo <89429924+RuloEncabo@users.noreply.github.com>
2024-08-07 13:56:48 -0300

    Update views.py

--- EltaRRHH/chofer/views.py
+++ EltaRRHH/chofer/views.py
@@ -1,6 +1,6 @@
 from datetime import date
 from django.shortcuts import get_object_or_404, render, redirect
-from .forms import ChoferForm
+from .forms import ChoferForm,ChoferVencimientoForm
 from .models import Chofer
 
 
@@ -43,68 +43,15 @@ def modidoc(request, chofer_id):
     chofer = get_object_or_404(Chofer, Id_chofer=chofer_id)
 
     if request.method == 'POST':
-        form = ChoferForm(request.POST, instance=chofer)
+        form = ChoferVencimientoForm(request.POST, instance=chofer)
         if form.is_valid():
             form.save()
+            print("Formulario guardado exitosamente")  # Debug
             return redirect('listdoc')  # Redirige a la lista después de guardar
+        else:
+            print("Formulario no es válido")  # Debug
+            print(form.errors)  # Mostrar errores del formulario para depuración
     else:
         form = ChoferForm(instance=chofer)
 
     return render(request, 'chofer/modidoc.html', {'form': form, 'chofer': chofer})
-"""
-def registro_chofer(request):
-    if request.method == 'POST':
-        form = ChoferForm(request.POST)
-        chofer_form = ChoferForm(request.POST, request.FILES)
-        if form.is_valid() and chofer_form.is_valid():
-            email = form.cleaned_data['email']
-            
-            user, created = Usuario.objects.get_or_create(
-                email=email,
-                defaults={
-                    'first_name': form.cleaned_data['first_name'],
-                    'last_name': form.cleaned_data['last_name'],
-                    'username': email.split("@")[0],
-                    'phone_number': form.cleaned_data['phone_number'],
-                    'password': form.cleaned_data['password']
-                }
-            )
-            
-            if created:
-                # Crear el chofer solo si el usuario es nuevo
-                chofer = chofer_form.save(commit=False)
-                chofer.usuario = user
-                chofer.save()
-
-                # Iniciar sesión automáticamente
-                login(request, user)
-
-                # Redirigir a una página de éxito
-                return redirect('success_page')  # Cambia 'success_page' por la URL adecuada
-            else:
-                # El usuario ya existe, no necesitas guardar los datos personales
-                chofer = chofer_form.save(commit=False)
-                chofer.usuario = user
-                chofer.save()
-                
-                # Redirigir o mostrar un mensaje que indique que el usuario ya está registrado
-                return redirect('success_page')  # Cambia 'success_page' por la URL adecuada
-
-    else:
-        form = ChoferForm()
-        chofer_form = ChoferForm()
-
-    context = {
-        'form': form,
-        'chofer_form': chofer_form,
-        'user_exists': False
-    }
-
-    # Verifica si el usuario ya existe basándote en el correo electrónico
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        if email and Usuario.objects.filter(email=email).exists():
-            context['user_exists'] = True
-
-    return render(request, 'chofer/regChoferistro.html', context)
-"""
