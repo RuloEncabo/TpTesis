4eb383e422d2abec6f21bd9a5f1f86744dfd0a7b
Marcelo Encabo <89429924+RuloEncabo@users.noreply.github.com>
2024-08-08 15:24:17 -0300

    Update views.py

--- EltaRRHH/chofer/views.py
+++ EltaRRHH/chofer/views.py
@@ -1,6 +1,7 @@
 from datetime import date
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render, redirect
-from .forms import ChoferForm,ChoferVencimientoForm
+from .forms import ChoferForm,ChoferVencimientoForm, ChofermodiForm
 from .models import Chofer
 
 
@@ -36,8 +37,6 @@ def listdoc(request):
     }
     return render(request, 'chofer/listdoc.html', context)
 
-
-
 ###  Funcion para modificar los Vencimientos ###
 def modidoc(request, chofer_id):
     chofer = get_object_or_404(Chofer, Id_chofer=chofer_id)
@@ -55,3 +54,48 @@ def modidoc(request, chofer_id):
         form = ChoferForm(instance=chofer)
 
     return render(request, 'chofer/modidoc.html', {'form': form, 'chofer': chofer})
+
+
+
+###  Funcion para modificar los Chofer ###
+def editarchofer(request, chofer_id):
+    chofer = get_object_or_404(Chofer, Id_chofer=chofer_id)
+
+    if request.method == 'POST':
+        form = ChofermodiForm(request.POST, instance=chofer)
+        if form.is_valid():
+            form.save()
+            print("Formulario guardado exitosamente")  # Debug
+            return redirect('listchofer')  # Redirige a la lista después de guardar
+        else:
+            print("Formulario no es válido")  # Debug
+            print(form.errors)  # Mostrar errores del formulario para depuración
+    else:
+        form = ChoferForm(instance=chofer)
+
+    return render(request, 'chofer/editarchofer.html', {'form': form, 'chofer': chofer})
+
+
+###  Funcion para modificar un chofer ###
+def editarchoferr(request, chofer_id):
+    chofer = get_object_or_404(Chofer, Id_chofer=chofer_id)
+    if request.method == 'POST':
+        form = ChofermodiForm(request.POST, instance=chofer)
+        if form.is_valid():
+            form.save()
+            return redirect('listusuariochofer')
+    else:
+        form = ChoferForm(instance=chofer)
+    return render(request, 'chofer/editarchofer.html', {'form': form})
+
+###  Funcion para borrar un chofer ###
+def Borrarchofer(request):
+    if request.method == 'POST':
+        chofer_id = request.POST.get('id')
+        try:
+            chofer = Chofer.objects.get(id=chofer_id)
+            chofer.delete()
+            return JsonResponse({'success': True})
+        except Chofer.DoesNotExist:
+            return JsonResponse({'success': False})
+    return JsonResponse({'success': False})
