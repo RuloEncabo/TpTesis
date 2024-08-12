52ea52b3dc847956480995e9a678acd1a227e72c
Marcelo Encabo <89429924+RuloEncabo@users.noreply.github.com>
2024-08-12 14:43:46 -0300

    Update urls.py

--- EltaRRHH/usuarios/urls.py
+++ EltaRRHH/usuarios/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('registro/', views.registro, name='registro'),     #funcion def que registra un usuario
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('sol_reset_pass/',views.sol_reset_pass, name='sol_reset_pass'),
     path('listar/', views.listar, name='listar'),
     #path('admin/', views.home, name='admin'),
     path('chofer/', views.chofer, name='chofer'),
@@ -16,7 +15,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'), # ruta para la funcion activate con los parametros  
     path('delete_usuario/', views.delete_usuario, name='delete_usuario'),
-    path('pedidocuenta/', views.pedidocuenta, name='pedidocuenta'),
-    
     
 ]
