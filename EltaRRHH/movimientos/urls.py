0881e5ea74a09d844d07efeb7c2474bfedd90138
Marcelo Encabo <89429924+RuloEncabo@users.noreply.github.com>
2024-08-13 12:42:42 -0300

    Update urls.py

--- EltaRRHH/movimientos/urls.py
+++ EltaRRHH/movimientos/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns = [
     path('registrarmovimiento/', views.registrarmovimiento, name='registrarmovimiento'),
     path('registrarmovimientoc/', views.registrarmovimientoc, name='registrarmovimientoc'),
+    path('inicioMov/', views.inicioMov, name='inicioMov'),
+    path('finMov/<int:mov_id>/', views.finMov, name='finMov'),
+    path('modificar/<int:id>/', views.modificarmovimiento, name='modificarmovimiento'),
+    path('borrarmovimiento/', views.borrarmovimiento, name='borrarmovimiento'),
     path('listmovimiento/', views.listmovimiento, name='listmovimiento'),
     path('movimiento/', views.movimiento, name='movimiento'),
     path('movchofer/', views.movchofer, name='movchofer'),
